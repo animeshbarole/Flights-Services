10ee863aae27aa1dd2212553abc347b3318adaae
Animesh <animeshdeas@gmail.com>
2023-06-26 07:26:24 +0530

    Created a new City Resource using npx sequelize model:generate --name city --attributes name:string npx sequelize db:migrate --> used to generate table;

---
+++ src/migrations/20230626014449-create-city.js
@@ -0,0 +1,31 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('cities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name:
+      { 
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique : true   //every city name should be unique
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('cities');
+  }
+};
