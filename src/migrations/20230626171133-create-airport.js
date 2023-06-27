63c5b7eb9b3f188f55f12c9d6aa388daa903f92a
Animesh <animeshdeas@gmail.com>
2023-06-27 17:03:29 +0530

    Added Airport model and migrations without FK constraints

---
+++ src/migrations/20230626171133-create-airport.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Airports', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull :false,
+        unique : true,
+      },
+      code: {
+        type: Sequelize.STRING,
+        allowNull :false,
+        unique : true,
+      },
+      address: {
+        type: Sequelize.STRING,
+        unique:true,
+      },
+      cityID: {
+        type: Sequelize.INTEGER,
+        allowNull :false
+
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Airports');
+  }
+};
