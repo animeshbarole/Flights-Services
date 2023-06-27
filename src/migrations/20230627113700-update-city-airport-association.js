1ac7bab57ea085f44ae0f6e8246d188e4cbb4522
Animesh <animeshdeas@gmail.com>
2023-06-27 18:56:01 +0530

    Added new migrations that can add a new coloumn of airport ````` npx sequelize migration:generate --name update-city-airport-association ```

---
+++ src/migrations/20230627113700-update-city-airport-association.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+     
+    await queryInterface.addConstraint('Airports',{
+
+       type:'FOREIGN KEY',
+       name:'city_fkey_constraint',
+       fields : ['cityID'],
+
+       references:{
+        table: 'Cities',
+        field :'id'
+       },
+       onDelete:'cascade',
+       onUpdate:'cascade'     
+     
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    
+
+    await queryInterface.removeConstraint('Airports','city_fkey_constraint');
+
+  }
+};
+
--- src/models/airport.js
+++ src/models/airport.js
@@ -10,7 +10,12 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      
+        this.belongsTo(models.City,{
+          foreignKey:'cityID',
+          onDelete:'CASCADE',
+          onUpdate:'CASCADE'
+        })
     }
   }
   Airport.init({
