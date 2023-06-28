fe4e72ffa9411cc4f0d196bda5ec932669becdd7
Animesh <animeshdeas@gmail.com>
2023-06-28 07:14:35 +0530

    Added Bad Codes  along with previous commit

--- src/index.js
+++ src/index.js
@@ -14,20 +14,38 @@ app.use('/api', apiRoutes);
 app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
 
-    const {Airport,City} = require('./models');
-   const city = await City.findByPk(10);
-   console.log(city);
-
-  //  const airport = await Airport.create({name :'DeviAhilya Airport',code :'IND'});
-  //  console.log(airport);
-     
-   // const response = await city.createAirport({name :'Bijasan Airport',code :'IND'});
-
-  await City.destroy({
-    where :{
-        id :10,
-    }
-  })
+   
+  
+    //Bad Code 
+    // const {Airport,City} = require('./models');
+    //const city = await City.findByPk(10);
+    //console.log(city);
+    // const bengaluru = await City.findByPk(1, {include: {model: Airport}});
+    // console.log(bengaluru);
+    // const airport = await Airport.create({name: 'Kempegowda Airport', code: 'BLR', cityId: 1});
+    // const dbpairport = await bengaluru.createAirport({name: 'Huballi Airport', code: 'HBL'});
+    // console.log(dbpairport);
+    // const airportsInBlr = await bengaluru.getAirports();
+    // console.log(airportsInBlr);
+    // const hbairport = await Airport.findByPk(3);
+    // console.log(hbairport);
+    // await bengaluru.removeAirports(hbairport);
+    // const mumbai = await City.findByPk(2);
+    // const sh = mumbai.createAirport({name: 'CSI airport', code: 'MUM'});
+    // await City.destroy({
+    //     where: {
+    //         id: 2
+    //     }
+    // });
+    // const sh = await Airport.findByPk(2);
+    // mumbai.removeAirport(sh)
+    // const city = await City.findByPk(4);
+    // await city.createAirport({name: 'Indore airport', code: 'IND'});
+    //  await City.destroy({
+    //    where :{
+    //        id :10,
+    //    }
+    //  })
 
  
 
